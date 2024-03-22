52269090e00dbef51502d097a3c4dc361b766a58
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-03-23 05:54:31 +0800

    Update the database name.

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,19 +1,19 @@
-import { DataSource } from "typeorm";
+import { DataSource } from 'typeorm';
 
 // Hard code the values.
 // Process ENV not working properly.
 export const connectionSource = new DataSource({
-  migrationsTableName: "migrations",
-  type: "mysql",
-  host: "localhost",
+  migrationsTableName: 'migrations',
+  type: 'mysql',
+  host: 'localhost',
   port: 3310,
-  username: "root",
-  password: "root",
-  database: "node-nest",
+  username: 'root',
+  password: 'root',
+  database: 'nodenest',
   logging: false,
   synchronize: false,
-  name: "default",
-  entities: ["dist/src/entities/*.js"],
-  migrations: ["dist/src/migrations/*.js"],
-  ssl: false,
+  name: 'default',
+  entities: ['dist/src/entities/*.js'],
+  migrations: ['dist/src/migrations/*.js'],
+  ssl: false
 });
