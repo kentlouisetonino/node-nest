58cecfaead124d99451a750631dfd41359f50532
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-03-23 12:51:14 +0800

    Create a user input data transfer object.

---
+++ src/user/dto/user.input.ts
@@ -0,0 +1,19 @@
+import { IsNotEmpty, IsEmail, IsString } from 'class-validator';
+
+export class CreateUserInput {
+  @IsNotEmpty()
+  @IsString()
+  firstName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  lastName: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+}
