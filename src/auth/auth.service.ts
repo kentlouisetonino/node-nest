f7fdea09ba123cd80a79c5ef13dbeb181c5a3062
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-03-23 15:24:51 +0800

    Create a loginUser handler in strategies.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
-import { AuthorizeTokenOutput } from './dto/auth.output';
+import { AuthorizeTokenOutput, LoginOutput } from './dto/auth.output';
+import { LoginInput } from './dto/auth.input';
 
 @Injectable()
 export class AuthService {
@@ -21,6 +22,39 @@ export class AuthService {
     return {
       accessToken: accessToken,
       currentUser: user
+    };
+  }
+
+  async loginUser(payload: LoginInput): Promise<LoginOutput> {
+    const user = await this.userService.getUserByEmail(payload.email);
+    const decodedPassword = this.jwtService.decode(user.password);
+
+    // Check if the user password is correct.
+    if (payload.password !== decodedPassword) {
+      throw new UnauthorizedException('Password is incorrect.');
+    }
+
+    const tokenPayload = {
+      id: user.id,
+      email: payload.email
+    };
+
+    // Create an accessToken.
+    const signedToken = this.jwtService.sign(tokenPayload, {
+      secret: `${this.configService.get('AUTH_JWT_SECRET')}`,
+      expiresIn: '25200s'
+    });
+
+    // Makes sure the user exist, then store the accessToken.
+    if (user) {
+      await this.userService.updateUser({
+        id: user.id,
+        accessToken: signedToken
+      });
+    }
+
+    return {
+      accessToken: signedToken
     };
   }
 }
