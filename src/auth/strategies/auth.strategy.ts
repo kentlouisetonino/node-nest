cd98e7948d5e06e66e8b05c4d48a81b071dba15e
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-03-23 15:32:56 +0800

    Create an auth strategy.

--- src/auth/strategies/auth.strategy.ts
+++ src/auth/strategies/auth.strategy.ts
@@ -1,12 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-jwt';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AuthService } from '../auth.service';
+import { UserService } from 'src/user/user.service';
+import { AuthorizeTokenOutput } from '../dto/auth.output';
 
 @Injectable()
 export class AuthStrategy extends PassportStrategy(Strategy) {
   constructor(
-    private readonly configService: ConfigService,
-    private readonly authService: AuthService
-  ) {}
+    configService: ConfigService,
+    private readonly authService: AuthService,
+    private readonly userService: UserService
+  ) {
+    super({
+      // Intercept the request if their is a bearer token in header.
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get('AUTH_JWT_SECRET')
+    });
+  }
+
+  // Validate the token pass by the client in the header.
+  async validate(validationPayload: {
+    email: string;
+  }): Promise<AuthorizeTokenOutput> {
+    const user = await this.userService.getUserByEmail(validationPayload.email);
+
+    // Check if user does not exist.
+    if (!user) {
+      throw new UnauthorizedException('Email does not exist.');
+    }
+
+    // If user exist, but does not have an accessToken token.
+    if (!user.accessToken) {
+      throw new UnauthorizedException('Invalid token. Please login again.');
+    }
+
+    return await this.authService.authorizeToken(
+      validationPayload.email,
+      user.accessToken
+    );
+  }
 }
